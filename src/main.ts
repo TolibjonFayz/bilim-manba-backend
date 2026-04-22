fbba23ecea513a448f21c9a678f910c3fb5c9db0
Tolibjon <tolibjonfayz@gmail.com>
2026-04-22 15:42:37 +0500

    Update CORS configuration to allow all origins for improved accessibility

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://www.bilimmanba.uz',
-    ],
+    origin: '*',
     methods: 'GET,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
